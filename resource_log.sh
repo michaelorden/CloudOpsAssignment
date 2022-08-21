2e9a9488670e5f93acf46ae1319576f8e995699d
Michael_Orden <110533700+michaelorden@users.noreply.github.com>
2022-08-21 14:59:53 +1200

    Update resource_log.sh

--- resource_log.sh
+++ resource_log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/bin/docker stats --format='{{json .}}' --no-stream > /root/log
-/usr/bin/docker cp /root/log 9f729129edc0:/usr/share/nginx/html/log.html
+/usr/bin/docker cp /root/log da07c746cb2d:/usr/share/nginx/html/log.html
